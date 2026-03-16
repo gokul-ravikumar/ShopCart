b66151af45d2654b81b54c6291cde009d8d4328b
gokul ravikumar <gokulravikumar.dev@gmail.com>
2026-03-16 16:12:33 +0530

    Remove unnecessary debug log from cart middleware

--- middlewares/cartMiddleware.js
+++ middlewares/cartMiddleware.js
@@ -2,7 +2,6 @@ const Cart = require("../models/Cart");
 const getCartItemCount = require("../helpers/cartHelper");
 
 const loadCartItemCount = async (req, res, next) => {
-  console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
   // Set default locals for boilerplate
   res.locals.hidePageFooter = false;
   
